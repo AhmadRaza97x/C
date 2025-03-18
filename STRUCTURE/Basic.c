5594b71dfb06d81e67c1e8237465811305b5798b
MOHAMMAD AHMAD RAZA <ahmadraza97x@gmail.com>
2025-03-18 15:08:19 +0530

    initial commit

--- STRUCTURE/Basic.c
+++ STRUCTURE/Basic.c
@@ -1,6 +1,39 @@
 #include<stdio.h>
+
+    // BASIC STRUCTURE
+
 int main()
 {
+    struct Pokemon  // USER DEFINE DATA TYPE
+    {
+       int hp;
+       int speed;
+       int attack;
+       char tier;
+    };
+
+    struct Pokemon Pikachu;
+    Pikachu.hp=50;
+    Pikachu.speed=100;
+    Pikachu.attack=60;
+    Pikachu.tier='A';
+
+    struct Pokemon Charizard;
+    Charizard.hp=80;
+    Charizard.speed=80;
+    Charizard.attack=130;
+    Charizard.tier='S';
+
+    struct Pokemon Mewtwo;
+    Mewtwo.hp=150;
+    Mewtwo.speed=200;
+    Mewtwo.attack=170;
+    Mewtwo.tier='G';
+
+    printf("%d",Charizard.attack);
+
+
+
     
     return 0;
 }
