184dd11b049e3e74eed9de8dfa6ab85ff18cf7d4
MOHAMMAD AHMAD RAZA <ahmadraza97x@gmail.com>
2025-04-12 14:42:02 +0530

    initial commit

---
+++ STRUCTURE/Cricketer.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+#include<string.h>
+
+    // CRICKETER DATA LIST
+
+int main(int argc, char const *argv[])
+{
+    typedef struct Cricketer
+    {
+        char firstname[15];
+        char lastname[15];
+        int age;
+        int No_Of_Matches;
+        float average_runs;
+
+    }Cricketer;
+
+// TAKING USER INPUT
+
+    Cricketer arr[3];
+    for (int i = 0; i < 3; i++)
+    {
+        printf("ENTER FIRST NAME : ");
+        scanf("%s",arr[i].firstname);
+        printf("ENTER LAST NAME : ");
+        scanf("%s",arr[i].lastname);
+        printf("ENTER AGE OF : ");
+        scanf("%d",&arr[i].age);
+        printf("ENTER NO. OF MATCHES : ");
+        scanf("%d",&arr[i].No_Of_Matches);
+        printf("ENTER AVERAGE RUNS : ");
+        scanf("%f",&arr[i].average_runs);
+        printf("\n");
+    }
+
+// PRINTING
+
+    for (int i = 0; i < 3; i++)
+    {
+        printf("\nNAME : %s %s\n",arr[i].firstname,arr[i].lastname);
+        printf("AGE : %d\n",arr[i].age);
+        printf("NO. OF MATCHES : %d\n",arr[i].No_Of_Matches);
+        printf("AVERAGE RUNS : %f\n",arr[i].average_runs);
+        printf("\n");
+    }
+    
+    return 0;
+}
