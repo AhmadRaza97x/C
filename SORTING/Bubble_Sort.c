4b7cc70ceec3d887a3f7940c9901f8f925e926c7
MOHAMMAD AHMAD RAZA <ahmadraza97x@gmail.com>
2025-04-18 03:02:33 +0530

    intial commit

---
+++ SORTING/Bubble_Sort.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+#include<stdbool.h>
+
+    // BUBBLE SORT
+
+int main()
+{
+    int arr[5] = {5,4,3,2,1};
+    int n = 5;
+
+// PRINTING
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ",arr[i]);
+    }
+
+// BUBBLE SORT
+    for (int i = 0; i < n-1; i++)
+    {
+        bool flag = true;
+        for (int j = 0; j < n-1-i; j++)
+        {
+            if (arr[j] > arr[j+1])
+            {
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+                flag = false;
+            }
+        }
+        if (flag == true)
+            break;
+    }
+
+// PRINTING
+    printf("\n");
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ",arr[i]);
+    }
+
+    return 0;
+}
