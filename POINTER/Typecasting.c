8524d98238112314e13e145a20df5c0c077344d4
MOHAMMAD AHMAD RAZA <ahmadraza>
2025-06-12 10:01:26 +0530

    initial commit

--- POINTER/Typecasting.c
+++ POINTER/Typecasting.c
@@ -1,9 +1,10 @@
 #include<stdio.h>
 int main()
 {
-    int x = 20;
+    // int x = 20;
     // int x = 300;
     // int x = 130;
+    int x = 400;
     char *p = (char*)&x;
     printf("%d",*p);
     return 0;
